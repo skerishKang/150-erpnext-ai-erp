91b88e90984e99961229a1e034a37d186179a854
skerishKang <198730@jnu.ac.kr>
2026-05-19 19:13:28 +0900

    test: convert permission tests to unittest

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -1,5 +1,7 @@
 """Tests for shared ERP permission helpers."""
 
+import unittest
+
 from frappe_stub import ensure_app_path, install_frappe_stub
 
 ensure_app_path()
@@ -9,14 +11,19 @@ from padiem_ai.erp.permissions import require_ceo_briefing_read_permission  # no
 from padiem_ai.erp.read_only_modules.constants import CEO_BRIEFING_READ_DOCTYPES  # noqa: E402
 
 
-def test_require_ceo_briefing_read_permission_checks_all_doctypes():
-    frappe.has_permission.reset_mock()
+class PermissionHelperTests(unittest.TestCase):
+    def test_require_ceo_briefing_read_permission_checks_all_doctypes(self):
+        frappe.has_permission.reset_mock()
 
-    require_ceo_briefing_read_permission()
+        require_ceo_briefing_read_permission()
 
-    assert frappe.has_permission.call_count == len(CEO_BRIEFING_READ_DOCTYPES)
-    checked_doctypes = [call.args[0] for call in frappe.has_permission.call_args_list]
-    assert checked_doctypes == list(CEO_BRIEFING_READ_DOCTYPES)
-    for call in frappe.has_permission.call_args_list:
-        assert call.args[1] == "read"
-        assert call.kwargs == {"throw": True}
+        self.assertEqual(frappe.has_permission.call_count, len(CEO_BRIEFING_READ_DOCTYPES))
+        checked_doctypes = [call.args[0] for call in frappe.has_permission.call_args_list]
+        self.assertEqual(checked_doctypes, list(CEO_BRIEFING_READ_DOCTYPES))
+        for call in frappe.has_permission.call_args_list:
+            self.assertEqual(call.args[1], "read")
+            self.assertEqual(call.kwargs, {"throw": True})
+
+
+if __name__ == "__main__":
+    unittest.main()
